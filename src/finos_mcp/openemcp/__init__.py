7933a5ed9eae0148305499fad2bcced3cb6e723c
Hugo Calderon <calderon.hugo@gmail.com>
2026-03-04 09:57:41 -0300

    feat: add canonical OpenEMCP validation status mapping

--- src/finos_mcp/openemcp/__init__.py
+++ src/finos_mcp/openemcp/__init__.py
@@ -1,9 +1,12 @@
 """Internal OpenEMCP compatibility primitives."""
 
 from .compat import OpenEMCPEnvelope, OpenEMCPPhase, create_envelope
+from .status import OpenEMCPValidationStatus, normalize_validation_status
 
 __all__ = [
     "OpenEMCPEnvelope",
     "OpenEMCPPhase",
+    "OpenEMCPValidationStatus",
     "create_envelope",
+    "normalize_validation_status",
 ]
